2f5fac038e3866a4a02ec04c9925fad2cb85e47b
jlsuarezdiaz <jlsuarezdiaz@ei140092>
2015-02-19 12:55:13 +0100

    LSI <3

--- Napakalaki/src/napakalaki/Napakalaki.java
+++ Napakalaki/src/napakalaki/Napakalaki.java
@@ -15,7 +15,7 @@ public class Napakalaki {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola GIT");
+        System.out.println("Hola GIT. I LOVE LSI.");
     }
     
 }
