4644b2f9a46b2433350afd10fb43e0809bea99fc
andreshp <andreshp9@gmail.com>
2015-02-19 12:50:04 +0100

    Author andreshp added

--- Napakalaki/src/napakalaki/Napakalaki.java
+++ Napakalaki/src/napakalaki/Napakalaki.java
@@ -15,7 +15,7 @@ public class Napakalaki {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola GIT");
     }
     
 }
