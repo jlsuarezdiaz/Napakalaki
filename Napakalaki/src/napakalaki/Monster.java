f4627670317c5074b6ec3314ea6d5c591b11646a
jlsuarezdiaz <jlsuarezdiaz@correo.ugr.es>
2015-03-05 13:24:52 +0100

    Commented Monster

--- Napakalaki/src/napakalaki/Monster.java
+++ Napakalaki/src/napakalaki/Monster.java
@@ -1,4 +1,4 @@
-//////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 // Authors: Juan Luis Suárez Díaz, Andrés Herrera Poyatos
 // Universidad de Granada, March, 2015
 // Programación Orientada a Objetos - Napakalaki
@@ -7,27 +7,58 @@
 package napakalaki;
 
 /**
- *
- * @author Juan Luis
+ * Monster Class.
+ * This class has all the information related to the monsters
+ * in Napakalaki game.
+ * @author andreshp, jlsuarez
  */
 public class Monster {
+    
+    //--------------- Private Attributes ---------------//
+    
+    /**
+     * Monter's name
+     */
     private String name;
     
+    /**
+     * Monster's level
+     */
     private int combatLevel;
     
+    /**
+     * Prize: consequences of defeating monster.
+     */
     private Prize prize;
     
+    /**
+     * Consequences of being defeated.
+     */
     private BadConsequence badCons;
     
-    
+    /**
+     * Get the name attribute
+     * @return name
+     */
     public String getName(){
         return this.name;
     }
     
+    /**
+     * Get the level attribute.
+     * @return combatLevel
+     */
     public int getCombatLevel(){
         return this.combatLevel;
     }
     
+    /**
+     * Constructor.
+     * @param name Monster's name.
+     * @param level Monster's level.
+     * @param bc Bad consequence.
+     * @param prize  Prize.
+     */
     public Monster(String name, int level, BadConsequence bc, Prize prize){
         this.name = name;
         this.combatLevel = level;
@@ -35,7 +66,11 @@ public class Monster {
         this.badCons = bc;
     }
     
-   public String toString(){
+    /**
+     * Gets a string with the monster's info.
+     * @return 
+     */
+    public String toString(){
        return "Name = " + this.name + " level = " + Integer.toString(combatLevel) + " prize = [" + prize.toString()
                + "] bad consequence = [" + badCons.toString() + "]";
    }
