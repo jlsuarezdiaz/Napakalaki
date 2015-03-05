2c11273b0b90b0339c43ff8e96fef5c2ea362998
jlsuarezdiaz <jlsuarezdiaz@correo.ugr.es>
2015-03-05 13:09:15 +0100

    Added enum TreasureKind

---
+++ Napakalaki/src/napakalaki/TreasureKind.java
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////////////////////////////////
+// Authors: Juan Luis Suárez Díaz, Andrés Herrera Poyatos
+// Universidad de Granada, March, 2015
+// Programación Orientada a Objetos - Napakalaki
+//////////////////////////////////////////////////////////////////////////////////////
+package napakalaki;
+
+/**
+ *
+ * @author Juan Luis
+ */
+public enum TreasureKind {
+    ARMOR, ONEHAND, BOTHHANDS, HELMET, SHOE, NECKLACE
+}
