a51edaab747b598f9667e3c413a5fafcf52c1570
andreshp <andreshp@ei142093>
2015-03-05 13:33:42 +0100

    Added 3 Monsters!

--- Napakalaki/src/napakalaki/TestNapakalaki.java
+++ Napakalaki/src/napakalaki/TestNapakalaki.java
@@ -3,7 +3,11 @@
 // Universidad de Granada, March, 2015
 // Programación Orientada a Objetos - Napakalaki
 //////////////////////////////////////////////////////////////////////////////////////
+
 package napakalaki;
+
+import java.util.ArrayList; // ArrayList included
+import java.util.Arrays;    // Arrays included
 
 /**
  *
@@ -11,10 +15,35 @@ package napakalaki;
  */
 public class TestNapakalaki {
     /**
-     * @param args the command line arguments
+     * Main for testing the program.
+     * @param args the command line arguments.
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        ArrayList<Monster> monsters = new ArrayList();
+        BadConsequence badConsequence;
+        Prize prize;
+        
+        //---------------- Added Monster ----------------//
+
+        // Monster: Byakhees de bonanza
+        badConsequence = new BadConsequence("Pierdes tu armadura visible y otra oculta", 0, 
+                new ArrayList(Arrays.asList(TreasureKind.ONEHAND)),
+                new ArrayList(Arrays.asList(TreasureKind.ONEHAND)));
+        prize = new Prize(2,1);
+        monsters.add(new Monster("Byakhees de bonanza", 8, badConsequence, prize));
+        
+        // Monster: El rey de rosa
+        badConsequence = new BadConsequence("Pierdes 5 niveles y 3 tesoros visibles", 5, 3, 0);
+        prize = new Prize(4,2);
+        monsters.add(new Monster("El rey de rosa", 13, badConsequence, prize));
+        
+        // Monster: Ángeles de la noche ibicenca
+        badConsequence = new BadConsequence("Te atrapan para llevarte de fiesta y te dejan caer "
+                + "en mitad del vuelo. Descarta 1 mano visible y 1 mano oculta", 0, 
+                new ArrayList(Arrays.asList(TreasureKind.ONEHAND)), 
+                new ArrayList(Arrays.asList(TreasureKind.ONEHAND)));
+        prize = new Prize(4,1);
+        monsters.add(new Monster("Ángeles de la noche ibicenca", 14, badConsequence, prize));
     }
     
 }
