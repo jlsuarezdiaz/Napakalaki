2313c05672b752fd87478a7f8bc0e4bb29ff3a97
andreshp <andreshp9@gmail.com>
2015-03-26 15:09:28 +0100

    First draft on Napakalaki Class.

--- Napakalaki/src/napakalaki/Napakalaki.java
+++ Napakalaki/src/napakalaki/Napakalaki.java
@@ -1,8 +1,9 @@
-//////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 // Authors: Juan Luis Suárez Díaz, Andrés Herrera Poyatos
 // Universidad de Granada, March, 2015
 // Programación Orientada a Objetos - Napakalaki
-//////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
 package napakalaki;
 
 /**
@@ -10,5 +11,74 @@ package napakalaki;
  * @author andreshp
  */
 public class Napakalaki {
+   
+    /**
+     * Instance of Napakalaki.
+     * 
+     * It is the only valid instance of Napakalaki.
+     * It can be obtained with getInstance.
+     */
+    private static Napakalaki instance = null;
+
+    /** 
+     * Napakalaki constructor.
+     * 
+     * It is private since we are not letting to create more than one
+     * instance. To get that instance use the method getInstance.
+     */
+    private Napakalaki() {
+    }
+
+    private void initPlayers(names : string []){
+        
+    }
+    private Player nextPlayer(){
+        
+    }
+    public Napakalaki getInstance(){
+        
+    }
+    public CombatResult combat(){
+        
+    }
+    public void discardVisibleTreasure(t : Treasure){
+        
+    }
+    public void discardHiddenTreasure(t : Treasure){
+        
+    }
+    public boolean makeTreasureVisible(t : Treasure){
+        
+    }
+    public boolean void buyLevels(visible : Treasure [], hidden : Treasure []){
+        
+    }
+    public void initGame(players : string []){
+        
+    }
+    public Player getCurrentPlayer(){
+        
+    }
+    public Monster getCurrentMonster(){
+        
+    }
+    public boolean canMakeTreasureVisible(t : Treasure){
+        
+    }
+    public Treasure [] void getVisibleTreasures(){
+        
+    }
+    public Treasure [] getHiddenTreasures(){
+        
+    }
+    public boolean nextTurn(){
+        
+    }
+    public boolean nextTurnAllowed(){
+        
+    }
+    public boolean endOfGame(result : CombatResult){
+        
+    }
     
 }
