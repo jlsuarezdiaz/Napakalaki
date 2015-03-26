52ae8b208249116fd278a6b65391c61b4e01f617
jlsuarezdiaz <jlsuarezdiaz@correo.ugr.es>
2015-03-26 12:27:20 +0100

    Completed Dice

--- Napakalaki/src/napakalaki/Dice.java
+++ Napakalaki/src/napakalaki/Dice.java
@@ -5,18 +5,39 @@
 //////////////////////////////////////////////////////////////////////////////////////
 package napakalaki;
 
+import java.util.Random;
+
 /**
  * Dice class.
  * This class represents a dice structure used in Napakalaki game.
  * @author Juan Luis
  */
 public class Dice {
+    /**
+     * Class Instance
+     */
     private static Dice instance = null;
     
+    /**
+     * Private constructor
+     */
+    private Dice(){
+        
+    }
+    
+    /**
+     * @brief gets a single instance of the class.
+     * @return class instance.
+     */
     public static Dice getInstance(){
         if(instance == null){
             instance = new Dice();
         }
         return instance;
+    }
+    
+    public int nextNumber(){
+       Random rand = new Random();
+       return rand.nextInt(6)+1;
     }
 }
