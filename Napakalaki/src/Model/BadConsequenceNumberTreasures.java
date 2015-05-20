38d1da2cef77c94ec1d9ed14e756086618897688
andreshp <andreshp9@gmail.com>
2015-05-20 13:11:39 +0200

    Adds specific methods.

--- Napakalaki/src/Model/BadConsequenceNumberTreasures.java
+++ Napakalaki/src/Model/BadConsequenceNumberTreasures.java
@@ -16,5 +16,47 @@ public class BadConsequenceNumberTreasures extends BadConsequence {
     public BadConsequenceNumberTreasures(String text, int levels, int nVisible,
             int nHidden){
         super(text,levels,nVisible,nHidden,null,null,false);
-    }    
+    }
+    
+    /**
+     * Subtracts the visible treasure given from the bad consequence.
+     * 
+     * The number of visible treasures is decremented. 
+     * 
+     * @param t Treasure to subtract.
+     */
+    @Override
+    public void subtractVisibleTreasure(Treasure t){
+        nVisibleTreasures -= nVisibleTreasures > 0? 1 : 0;
+    }
+    
+    /**
+     * Subtracts the hidden treasure given from the bad consequence.
+     * 
+     * The number of hidden treasures is decremented. 
+     * 
+     * @param t Treasure to subtract.
+     */
+    public void subtractHiddenTreasure(Treasure t){
+        nHiddenTreasures -= nHiddenTreasures > 0? 1 : 0;
+    }
+    
+    /**
+     * Returns a string with the Bad Consequence instance contents.
+     * @return String with the contents.
+     */
+    @Override
+    public String toString(){
+
+        // Read only variable with the number of spaces to print in each line.
+        final int num_spaces = 5;
+        String strSpaces = new String(new char[num_spaces]).replace('\0', ' ');
+        
+        return super.toString() 
+               + strSpaces + "Text = " + text 
+               + "\n"+ strSpaces + "Levels = " + Integer.toString(levels)
+               + "\n"+ strSpaces + "nVisibleTreasures = " + Integer.toString(nVisibleTreasures)
+               + "\n"+ strSpaces + "nHiddenTreasures = " + Integer.toString(nHiddenTreasures);
+    }
+
 }
