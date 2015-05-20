ae8e5da03755d3dba72741ec4a943f6acb98d759
andreshp <andreshp9@gmail.com>
2015-05-20 13:11:24 +0200

    Adds specific methods

--- Napakalaki/src/Model/BadConsequenceSpecificTreasures.java
+++ Napakalaki/src/Model/BadConsequenceSpecificTreasures.java
@@ -21,4 +21,68 @@ public class BadConsequenceSpecificTreasures extends BadConsequence {
         super(text,levels,0,0,tVisible,tHidden,false);
     }
     
+    /**
+     * Subtracts the visible treasure given from the bad consequence.
+     * 
+     * Subtract t.getType() from the specific treasures
+     * asked.
+     * 
+     * @param t Treasure to subtract.
+     */
+    @Override
+    public void subtractVisibleTreasure(Treasure t){
+        specificVisibleTreasures.remove(t.getType());
+    }
+
+    /**
+     * Subtracts the hidden treasure given from the bad consequence.
+     * 
+     * Subtract t.getType() from the specific treasures
+     * asked.
+     * 
+     * @param t Treasure to subtract.
+     */
+    @Override
+    public void subtractHiddenTreasure(Treasure t){
+        specificVisibleTreasures.remove(t.getType());
+    }
+
+    /**
+     * Returns a string with the Bad Consequence instance contents.
+     * @return String with the contents.
+     */
+    public String toString(){
+        // Read only variable with the number of spaces to print in each line.
+        final int num_spaces = 5;
+        
+        String strSpaces = new String(new char[num_spaces]).replace('\0', ' ');
+        String specificVisibleTreasures_str = "", specificHiddenTreasures_str = "";
+        
+        // Get the specific visible treasures as string
+        if (specificVisibleTreasures == null || specificVisibleTreasures.isEmpty()){
+            specificVisibleTreasures_str = " Ninguno";
+        }
+        else{
+            for (TreasureKind treasure : specificVisibleTreasures){
+                specificVisibleTreasures_str += " " + treasure.name();
+            }
+            
+        }
+        
+        // Get the specific hidden treasures as string
+        if (specificHiddenTreasures == null || specificHiddenTreasures.isEmpty()){
+            specificHiddenTreasures_str = " Ninguno";
+        }
+        else{
+            for (TreasureKind treasure : specificHiddenTreasures){
+                specificHiddenTreasures_str += " " + treasure.name();
+            }
+            
+        }
+
+        return strSpaces + "Text = " + text 
+               + "\n"+ strSpaces + "Levels = " + Integer.toString(levels)
+               + "\n"+ strSpaces + "specificVisibleTreasures = " + specificVisibleTreasures_str
+               + "\n"+ strSpaces + "specificHiddenTreasures = "  + specificHiddenTreasures_str;
+    }
 }
