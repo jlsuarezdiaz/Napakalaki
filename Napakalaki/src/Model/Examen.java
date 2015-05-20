3a706d648983ed269e68b1f6bf5e78b2ffa0ff13
andreshp <andreshp9@gmail.com>
2015-05-20 17:37:02 +0200

    Adds Examen Class

---
+++ Napakalaki/src/Model/Examen.java
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////////////
+// Authors: Juan Luis Suárez Díaz, Andrés Herrera Poyatos
+// Universidad de Granada, March, 2015
+// Programación Orientada a Objetos - Napakalaki
+////////////////////////////////////////////////////////////////////////////////
+
+package Model;
+
+import java.util.ArrayList; // ArrayList included
+import java.util.Collections;
+
+/**
+ * Clase para el examen práctico de PDOO.
+ * @author andreshp
+ */
+public class Examen {
+
+    //---------------------- TESOROS ----------------------//
+    
+    //----------------- BAD CONSEQUENCES -----------------//
+    
+}
