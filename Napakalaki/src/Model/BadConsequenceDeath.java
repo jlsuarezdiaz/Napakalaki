7f8eb7f03a310f269bb54d036cbcf88e5f661a25
andreshp <andreshp9@gmail.com>
2015-05-20 13:12:13 +0200

    Adds some specific methods

--- Napakalaki/src/Model/BadConsequenceDeath.java
+++ Napakalaki/src/Model/BadConsequenceDeath.java
@@ -19,4 +19,39 @@ public class BadConsequenceDeath extends BadConsequence {
         super(text,0,0,0,null,null,death);
     }
     
+    /**
+     * Subtracts the visible treasure given from the bad consequence.
+     * 
+     * Do nothing since it is a death bad consequence.
+     * 
+     * @param t Treasure to subtract.
+     */
+    @Override
+    public void subtractVisibleTreasure(Treasure t){
+    }
+
+    /**
+     * Subtracts the hidden treasure given from the bad consequence.
+     * 
+     * Do nothing since it is a death bad consequence.
+     * 
+     * @param t Treasure to subtract.
+     */
+    @Override
+    public void subtractHiddenTreasure(Treasure t){
+    }
+
+    /**
+     * Returns a string with the Bad Consequence instance contents.
+     * @return String with the contents.
+     */
+    public String toString(){
+        // Read only variable with the number of spaces to print in each line.
+        final int num_spaces = 5;
+        String strSpaces = new String(new char[num_spaces]).replace('\0', ' ');
+
+        return strSpaces + "Text = " + text 
+               + "\n"+ strSpaces + "Levels = " + Integer.toString(levels)
+               + "\n"+ strSpaces + "Death = " + Boolean.toString(death);
+    }
 }
