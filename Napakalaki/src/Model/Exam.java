5a5f614bfcc5e3029357e5fbff512abb075ddaef
andreshp <andreshp9@gmail.com>
2015-06-15 19:40:13 +0200

    Exam Main

---
+++ Napakalaki/src/Model/Exam.java
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+// Authors: Juan Luis Suárez Díaz, Andrés Herrera Poyatos
+// Universidad de Granada, March, 2015
+// Programación Orientada a Objetos - Napakalaki
+////////////////////////////////////////////////////////////////////////////////
+
+package Model;
+
+import GUI.ExamView;
+
+/**
+ *
+ * @author andreshp
+ */
+public class Exam {
+    public static void main(String[] args) {
+        ExamView examView = new ExamView();
+        examView.showView(); 
+    }    
+}
